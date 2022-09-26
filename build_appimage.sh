6220c485c776192059ad6ad0e0de77cfaeb2ced9
zhangfuwen <zhangfuwen@bytedance.com>
2022-09-26 21:43:12 +0800

    update ci

--- build_appimage.sh
+++ build_appimage.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-mkdir build && cd build
+[[ ! -d build ]] && mkdir build
+cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr -G "Ninja"
 ninja
 DESTDIR=AppDir ninja install
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20220822-1/linuxdeploy-x86_64.AppImage
 chmod +x ./linuxdeploy-x86_64.AppImage
-./linuxdeploy-x86_64.AppImage --appdir ./AppDir --output appimage -o FunTerm.AppImage
+./linuxdeploy-x86_64.AppImage --appdir ./AppDir --output appimage # -o FunTerm.AppImage
 rm ./linuxdeploy-x86_64.AppImage
